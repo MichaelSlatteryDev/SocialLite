092c7c4d7a8a03b971ef4f493c9656997ba9c566
Michael Slattery <michaelslatterydev@gmail.com>
2022-06-12 12:31:03 +0100

    Add base XCode project

---
+++ SocialLite/SocialLite/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SocialLite
+//
+//  Created by Michael Slattery on 12/06/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
